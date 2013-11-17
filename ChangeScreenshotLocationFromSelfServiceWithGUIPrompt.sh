f708a8eb558eb287723d21733639f81dc35c525b
macmule <bentoms@btopenworld.com>
2013-11-17 22:18:35 +0000

    Update ChangeScreenshotLocationFromSelfServiceWithGUIPrompt.sh

--- ChangeScreenshotLocationFromSelfServiceWithGUIPrompt.sh
+++ ChangeScreenshotLocationFromSelfServiceWithGUIPrompt.sh
@@ -1,38 +1,20 @@
-#!/bin/sh
-####################################################################################################
-#
-# This is free and unencumbered software released into the public domain.
-# 
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-# 
-# In jurisdictions that recognise copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-# 
-# For more information, please refer to <http://unlicense.org/>
-#
+#!/bin/bash
 ####################################################################################################
 #
 # More information: http://macmule.com/2013/07/13/change-screenshot-location-from-self-service-with-gui-prompt/
 #
 # GitRepo: https://github.com/macmule/Change-Screenshot-Location-From-Self-Service-With-GUI-Prompt
 #
+# License: http://macmule.com/license/
+#
+###################################################################################################
+#
 ####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
 
 ###
 # Get the Username of the logged in user
@@ -61,5 +43,4 @@ chown "$loggedInUser" /Users/"$loggedInUser"/Library/Preferences/com.apple.scree
 # Restart SystemUIServer for changes to take affect
 killall SystemUIServer
 
-# Exit
 exit 0
